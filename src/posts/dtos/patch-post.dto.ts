4df5b97ffb4dd005db49e26ad02b2f89d02700e0
shubham-crest <shubham.t@crestinfosystems.com>
2024-11-25 14:21:59 +0530

    added patch post dto using PartialType

---
+++ src/posts/dtos/patch-post.dto.ts
@@ -0,0 +1,13 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreatePostDto } from './create-post.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty } from 'class-validator';
+
+export class PatchPostDto extends PartialType(CreatePostDto) {
+  @ApiProperty({
+    description: 'The ID of the post that needs to be updated',
+  })
+  @IsInt()
+  @IsNotEmpty()
+  id: number;
+}
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -13,8 +14,28 @@ export class PostsController {
     return this.postsService.findAll(userId);
   }
 
+  @ApiOperation({
+    summary: 'Creates a new blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 reponse if your post is created successfully',
+  })
   @Post()
   public createPost(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto);
+  }
+
+
+  @ApiOperation({
+    summary: 'Updates an existing blog post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'You get a 200 reponse if your post is updated successfully',
+  })
+  @Patch()
+  public updatePost(@Body() patchPostsDto: PatchPostDto) {
+    console.log('patchPostsDto', patchPostsDto);
   }
 }
