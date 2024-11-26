4a8706efbfd4fa78123f437575ec19ee46d410f6
shubham-crest <shubham.t@crestinfosystems.com>
2024-11-26 10:42:33 +0530

    added one to one relationship between post and meta option entities

--- src/posts/post.entity.ts
+++ src/posts/post.entity.ts
@@ -1,7 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { postType } from './enums/postType.enum';
 import { postStatus } from './enums/postStatus.enum';
-import { CreatePostMetaOptionsDto } from '../meta-options/dtos/create-post-meta-options.dto';
+import { MetaOption } from 'src/meta-options/meta-option.entity';
 
 @Entity()
 export class Post {
@@ -42,6 +42,9 @@ export class Post {
   @Column({ type: 'timestamp', nullable: true })
   publishedOn?: Date;
 
+  @OneToOne(() => MetaOption)
+  @JoinColumn()
+  metaOptions?: MetaOption;
+
   tags?: string[];
-  metaOptions?: CreatePostMetaOptionsDto[];
 }
