35625c11018ccd93759f73d12e4de67a5addd3f1
shubham-crest <shubham.t@crestinfosystems.com>
2024-11-26 17:46:52 +0530

    added @nestjs/config for managing ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,41 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { TagsModule } from './tags/tags.module';
+import { MetaOptionsModule } from './meta-options/meta-options.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        // entities: [User],
+        autoLoadEntities: true,
+        synchronize: true,
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        host: configService.get('DATABASE_HOST'),
+        database: configService.get('DATABASE_NAME'),
+      }),
+    }),
+    TagsModule,
+    MetaOptionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
